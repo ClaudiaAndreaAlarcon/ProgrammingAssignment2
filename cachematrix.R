65dcc7245e7c3c3a3f4aaadcb570853b83e2acbf
ClaudiaAndreaAlarcon <claudia.andrea.alarcon@gmail.com>
2021-01-20 15:39:41 -0400

    Tarea de Semana 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,18 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+        i<- NULL                                ## Para iniciar la inversa de la matriz
+        set<- function(matrix){                 ## Para establecer la matriz
+                m<<-matrix
+                i<<-NULL
+        }
+        get<- function() {                      ## Para obtener la matriz
+                m
+        }
+        setInverse<- function(inverse) {        ## Para establecer la inversa
+                i<<- inverse 
+        }
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m<- x$getinverse()
+        if (!is.null(m)) {
+                message("Obteniendo datos del cache")
+                return(m)
+        }
+        data<- x$get()
+        m<- solve(data)%*% data
+        x$setInverse(m)
+        m
 }
